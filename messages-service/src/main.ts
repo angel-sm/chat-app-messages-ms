c71af0f9c78eedc0896b944ab0387ee1823a4181
angel-sm <jose.angel.pacheco9729@gmail.com>
2025-04-02 14:52:46 -0600

    ci/cd test

--- messages-service/src/main.ts
+++ messages-service/src/main.ts
@@ -1,8 +1,6 @@
-
-
-export const handler = () => {
+export const handler = async (event: any) => {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello, world!' }),
+    body: JSON.stringify({ message: "Hello, World!" }),
   };
-}
+};
